4a4f5800a62cb3d9b970e841cac391b985fa23de
Darya Ibrahim <daryabsb@gmail.com>
2025-02-02 16:34:39 +0300

    working on streaming live data

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 from binance.client import Client
+from binance import ThreadedWebsocketManager
+import pandas as pd
 from BinanceKeys import api_key, secret_key
 import time
-import pandas as pd
-
+from datetime import datetime, timedelta
 client = Client(api_key, secret_key, tld='com')
 
 
@@ -43,23 +44,73 @@ bars = client.get_historical_klines(symbol = "BTCUSDT",
 df = pd.DataFrame(bars)
 df["Date"] = pd.to_datetime(df.iloc[:,0], unit = "ms")
 df.columns = ["Open Time", "Open", "High", "Low", "Close",
-              "Volume", "Clos Time", "Quote Asset Volume", 
-              "Number of Trades", "Taker Buy Base Asset Volume",
-              "Taker Buy Quote Asset Volume", "Ignore", "Date" ]
+                "Volume", "Clos Time", "Quote Asset Volume", 
+                "Number of Trades", "Taker Buy Base Asset Volume",
+                "Taker Buy Quote Asset Volume", "Ignore", "Date" ]
+
 df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
 
 df.set_index("Date", inplace = True)
 
+# valid intervals - 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
+def get_history(symbol, interval, start, end = None):
+    bars = client.get_historical_klines(symbol = symbol, interval = interval,
+                                        start_str = start, end_str = end, limit = 1000)
+    df = pd.DataFrame(bars)
+    df["Date"] = pd.to_datetime(df.iloc[:,0], unit = "ms")
+    df.columns = ["Open Time", "Open", "High", "Low", "Close", "Volume",
+                    "Clos Time", "Quote Asset Volume", "Number of Trades",
+                    "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore", "Date"]
+    df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
+    df.set_index("Date", inplace = True)
+    for column in df.columns:
+        df[column] = pd.to_numeric(df[column], errors = "coerce")
+    
+    return df
 
 def pr(command):
     print(command)
 
 
-for column in df.columns:
-    df[column] = pd.to_numeric(df[column], errors = "coerce")
-    
-pr(df) # current price for one symbol
+# for column in df.columns:
+#     df[column] = pd.to_numeric(df[column], errors = "coerce")
 
+# df = get_history(symbol = "BTCUSDT", interval = "1d", start = timestamp)
+
+# pr(df) # current price for one symbol
+now = datetime.utcnow()
+baghdad_time = now + timedelta(hours = 3)
+baghdad_time_last_night = baghdad_time - timedelta(hours = 13)
+
+df = get_history(symbol = "DOGEUSDT", interval = "1h", start = str(baghdad_time_last_night), end = str(baghdad_time))
+
+
+def stream_data(msg):
+    print(msg)
+
+
+# pr(baghdad_time) # current price for one symbol
+# pr(baghdad_time_last_night) # current price for one symbol
+# pr(df) # current price for one symbol
+
+twm = ThreadedWebsocketManager()
+{
+    'e': '24hrMiniTicker', 
+    'E': 1738502987558, 
+    's': 'DOGEUSDT', 
+    'c': '0.29401000', 
+    'o': '0.32444000', 
+    'h': '0.32447000', 
+    'l': '0.28848000', 
+    'v': '1718375686.00000000', 
+    'q': '523393611.91487000'
+}
+
+while True:
+    twm.start()
+    twm.start_symbol_miniticker_socket(callback = stream_data, symbol = "DOGEUSDT")
+    time.sleep(15)
+    twm.stop()
 
 
 
