042ad5af53bc0d7ab894e5c6c7ab464b05a428d9
Darya Ibrahim <daryabsb@gmail.com>
2025-03-17 16:46:50 +0300

    combined tasks

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -18,7 +18,73 @@ def test_tasks():
     return 'Bravo!!'
 
 
-@shared_task
+# @shared_task
+def update_usd_value():
+    from src.market.utils import get_total_usd
+    total_usd = get_total_usd()
+
+    group_name = 'total_usd_update'
+    message_type = 'total_usd'
+
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            'type': message_type,
+            'data': str(total_usd),  # Convert datetime to string
+        }
+    )
+
+
+# @shared_task
+def update_symbols():
+    from django.forms import model_to_dict
+    Symbol = apps.get_model('market', 'Symbol')
+    Kline = apps.get_model('market', 'Kline')
+
+    symbols = Symbol.objects.filter(active=True)
+
+    for symbol in symbols:
+        last_kline = Kline.objects.filter(symbol=symbol.pair).last()
+        try:
+            symbol.price = last_kline.close
+            symbol.volume_24h = str(last_kline.volume)
+            symbol.save()
+        except Exception as e:
+            print(f"{symbol.ticker}: {e}")
+    return True
+
+    # json_path = os.path.join(settings.BASE_DIR, 'trading', 'data', 'coins.json')
+    # if not os.path.exists(json_path):
+    #     return
+
+    # with open(json_path, 'r') as f:
+    #     coins = json.load(f)
+
+    # for coin_data in coins:
+    #     symbol = Symbol.objects.filter(ticker=coin_data['ticker']).first()
+    #     if symbol:
+    #         # Update fields
+    #         symbol.rank = coin_data['rank']
+    #         symbol.price = coin_data['price']
+    #         symbol.change_24h = coin_data['change_24h']
+    #         symbol.market_cap = coin_data['market_cap']
+    #         symbol.volume_24h = coin_data['volume24h']
+    #         symbol.circ_supply = coin_data['circ_supply']
+    #         symbol.save()  # Triggers WorkflowMixin.notify if included
+
+    #         # Update categories
+    #         symbol.categories.clear()
+    #         category_names = coin_data['category'].split(', ')
+    #         for cat_name in category_names:
+    #             category, _ = CryptoCategory.objects.get_or_create(
+    #                 name=cat_name,
+    #                 defaults={'slug': slugify(cat_name), 'rank': symbol.rank}
+    #             )
+    #             symbol.categories.add(category)
+
+
+# @shared_task
 def update_klines(symbols=None):
     """Update Kline data for the last 15 minutes."""
     import zoneinfo
@@ -105,69 +171,13 @@ def update_klines(symbols=None):
         except Exception as e:
             print(f"Error updating kline for {symbol_full}: {e}")
 
-    return 'Done updating klines'
+    return True
 
 
 @shared_task
-def update_usd_value():
-    from src.market.utils import get_total_usd
-    total_usd = get_total_usd()
-
-    group_name = 'total_usd_update'
-    message_type = 'total_usd'
-
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        group_name,
-        {
-            'type': message_type,
-            'data': str(total_usd),  # Convert datetime to string
-        }
-    )
-
-
-@shared_task
-def update_symbols():
-    from django.forms import model_to_dict
-    Symbol = apps.get_model('market', 'Symbol')
-    Kline = apps.get_model('market', 'Kline')
-
-    symbols = Symbol.objects.filter(active=True)
-
-    for symbol in symbols:
-        last_kline = Kline.objects.filter(symbol=symbol.pair).last()
-        try:
-            symbol.price = last_kline.close
-            symbol.volume_24h = str(last_kline.volume)
-            symbol.save()
-        except Exception as e:
-            print(f"{symbol.ticker}: {e}")
-
-    # json_path = os.path.join(settings.BASE_DIR, 'trading', 'data', 'coins.json')
-    # if not os.path.exists(json_path):
-    #     return
-
-    # with open(json_path, 'r') as f:
-    #     coins = json.load(f)
-
-    # for coin_data in coins:
-    #     symbol = Symbol.objects.filter(ticker=coin_data['ticker']).first()
-    #     if symbol:
-    #         # Update fields
-    #         symbol.rank = coin_data['rank']
-    #         symbol.price = coin_data['price']
-    #         symbol.change_24h = coin_data['change_24h']
-    #         symbol.market_cap = coin_data['market_cap']
-    #         symbol.volume_24h = coin_data['volume24h']
-    #         symbol.circ_supply = coin_data['circ_supply']
-    #         symbol.save()  # Triggers WorkflowMixin.notify if included
-
-    #         # Update categories
-    #         symbol.categories.clear()
-    #         category_names = coin_data['category'].split(', ')
-    #         for cat_name in category_names:
-    #             category, _ = CryptoCategory.objects.get_or_create(
-    #                 name=cat_name,
-    #                 defaults={'slug': slugify(cat_name), 'rank': symbol.rank}
-    #             )
-    #             symbol.categories.add(category)
+def update_system_data():
+    klines_updated = update_klines()
+    if klines_updated:
+        symbols_updated = update_symbols()
+        if total_usd:
+            total_usd = update_usd_value()
